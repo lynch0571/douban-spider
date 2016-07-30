a83eb3c4b794d301f0ed58f2b37b68368b84c3d8
lynch <lynch0571@gmail.com>
2016-07-30 18:12:35 +0800

    直接调用豆瓣API

--- douban/src/test/java/com/spider/douban/AppTest.java
+++ douban/src/test/java/com/spider/douban/AppTest.java
@@ -1,38 +1,43 @@
 package com.spider.douban;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.IOException;
+import java.util.Map;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	private static String urlBase = "https://api.douban.com/";
+	private static String urlVersion = "v2/";
+
+	@Test
+	public void printMovieSubject() throws IOException {
+		String urlBody = "movie/subject/";
+		String urlNum = "1764796";
+		String jsonStr = getHTMLDoc(urlBody, urlNum).body().html();
+		System.out.println(jsonStr);
+		printJson(jsonStr);
+	}
+
+	public Document getHTMLDoc(String urlBody, String urlNum)
+			throws IOException {
+		return Jsoup.connect(urlBase + urlVersion + urlBody + urlNum)
+				.ignoreContentType(true).get();
+	}
+
+	public void printJson(String jsonStr) throws JsonParseException,
+			JsonMappingException, IOException {
+		Map<String, String> map = new ObjectMapper().readValue(jsonStr,
+				Map.class);
+		System.out.println("---------------------------");
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
+		System.out.println("---------------------------");
+	}
 }
